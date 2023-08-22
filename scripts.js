7fb78f3eddec27e6eb37bd54b78cf5f6c398067e
harsh-100 <26harshagarwal@gmail.com>
2023-08-22 08:51:01 +0530

    adding the student record code

---
+++ scripts.js
@@ -0,0 +1,6 @@
+let fruits = ["apple", "banana", "orange"];
+console.log(fruits.length); // Array length
+console.log(fruits[0]); // Accessing an element
+fruits.push("grape"); // Adding an element
+
+
